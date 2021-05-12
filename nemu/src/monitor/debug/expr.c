1e69bcadc8d400909a0a9c91713ead8a759b10cd
zhenwei duan <dzwduan@gmail.com>
2021-05-12 11:30:20 +0800

    finish expr-gen

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -6,7 +6,7 @@
 #include <regex.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ,
+  TK_NOTYPE = 256, TK_EQ,TK_DEC,TK_HEX
 
   /* TODO: Add more token types */
 
@@ -24,6 +24,13 @@ static struct rule {
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
   {"==", TK_EQ},        // equal
+  {"-",  '-'},
+  {"\\*",'*'},
+  {"/",'/'},
+  {"\\(",'('},
+  {"\\)",')'},
+  {"0|([1-9][0-9]*)",TK_DEC},
+  {"0[Xx][0-9A-Fa-f]+",TK_HEX},
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
@@ -47,15 +54,21 @@ void init_regex() {
   }
 }
 
+//用于记录token信息
 typedef struct token {
   int type;
+  //记录子串
   char str[32];
 } Token;
 
+//attribute__((used))的函数被标记在目标文件中，以避免链接器删除未使用的节。
+//存放已经被识别出来的token信息
 static Token tokens[32] __attribute__((used)) = {};
+//已经被识别出的token数目
 static int nr_token __attribute__((used))  = 0;
-
+//e是输入字符串
 static bool make_token(char *e) {
+  //当前处理到的位置
   int position = 0;
   int i;
   regmatch_t pmatch;
@@ -63,7 +76,7 @@ static bool make_token(char *e) {
   nr_token = 0;
 
   while (e[position] != '\0') {
-    /* Try all rules one by one. */
+    //依次尝试所有规则
     for (i = 0; i < NR_REGEX; i ++) {
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
         char *substr_start = e + position;
@@ -78,9 +91,33 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
-
+        //记录到tokens
         switch (rules[i].token_type) {
-          default: TODO();
+          //空格不必记录
+          case TK_NOTYPE:break;
+          //下面的只需要记录类型
+          case '+':
+          case '-':
+          case '*':
+          case '/':
+          case '(':
+          case ')':
+            tokens[nr_token].type = rules[i].token_type;
+            nr_token++;
+            break;
+          //记录类型+字符串
+          case TK_HEX:
+          case TK_DEC:
+          case TK_EQ:
+            tokens[nr_token].type = rules[i].token_type;
+            memset(tokens[nr_token].str,0,32);
+            assert(strlen(substr_start)<32);
+            memcpy(tokens[nr_token].str,substr_start);
+            nr_token++;
+            break;
+
+          default: 
+           break;
         }
 
         break;
@@ -96,15 +133,80 @@ static bool make_token(char *e) {
   return true;
 }
 
+//======================helper functions==================
+
+static bool check_parentheses(int p,int q){
+  //出现（ +1
+  //出现 ）-1
+  //sum < 0 fasle
+  //final sum!=0 false
+  int sum = 0;
+  int i=p;
+  for(;i<q;i++){
+    if(tokens[p].type=='(') sum++;
+    if(tokens[p].type==')') sum--;
+    if(sum<0) return false;
+  }
+  return sum==0;
+}
+
+static int getMainOpt(int p,int q){
+
+}
+
+//p和q指示表达式开始和结束位置
+static word_t eval(int p, int q) {
+  if (p > q) {
+    /* Bad expression */
+    printf("Bad expression\n");
+    return 0;
+  }
+  else if (p == q) {
+    /* Single token.
+     * For now this token should be a number.
+     * Return the value of the number.
+     */
+    if(tokens[p].type==TK_DEC)
+      return atoi(tokens[p].str);
+    if(tokens[p].type==TK_HEX){
+      word_t num = 0;
+      sscanf(tokens[p].str,"0x%x",&num);
+      return num;
+    }
+  }
+  else if (check_parentheses(p, q) == true) {
+    /* The expression is surrounded by a matched pair of parentheses.
+     * If that is the case, just throw away the parentheses.
+     */
+    return eval(p + 1, q - 1);
+  }
+  else {
+    /* We should do more things here. */
+    int op = getMainOpt();
+    word_t val1 = eval(p, op - 1);
+    word_t val2 = eval(op + 1, q);
+
+    switch (tokens[op].type) {
+      case '+': return val1 + val2;
+      case '-': return val1 - val2;
+      case '*': return val1 * val2;
+      case '/': return val1 / val2;
+      default: 
+        printf("can not recognition type\n");
+        break;
+  }
+}
 
 word_t expr(char *e, bool *success) {
+  //check make token
   if (!make_token(e)) {
     *success = false;
     return 0;
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  TODO();
+
+
 
   return 0;
 }
--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <assert.h>
 #include <string.h>
+#include <stdarg.h>
 
 // this should be enough
 static char buf[65536] = {};
@@ -16,21 +17,62 @@ static char *code_format =
 "  return 0; "
 "}";
 
+int idx=0;
+
+#define SPACE do{if(rand()%2==0) gen(' ');}while(0)
+
+static uint32_t choose(int n){
+  return rand()%n;
+}
+
+void gen(char c){
+  buf[idx++] = c;
+}
+
+//生成最多两位数
+void gen_num(){
+  int num = choose(100);
+  if(num>=10){
+    buf[idx++] = num/10 + '0';
+    buf[idx++] = num%10 + '0';
+  }
+  else{
+    buf[idx++]=num+'0';
+  }
+}
+
+void gen_rand_op(){
+  switch(choose(3)){
+    case 0: gen('+'); break;
+    case 1: gen('-'); break;
+    case 2: gen('*'); break;
+    case 3: gen('/'); break;
+    default:break;
+  }
+}
+
 static inline void gen_rand_expr() {
-  buf[0] = '\0';
+  switch (choose(3)) {
+    case 0: gen_num(); break;
+    case 1: gen('('); SPACE; gen_rand_expr(); SPACE; gen(')'); break;
+    default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+  }
 }
 
 int main(int argc, char *argv[]) {
   int seed = time(0);
   srand(seed);
-  int loop = 1;
+  int loop = 10;
   if (argc > 1) {
     sscanf(argv[1], "%d", &loop);
   }
   int i;
   for (i = 0; i < loop; i ++) {
+    //每次循环都要将buf设置为初始状态
+    idx=0;
+    memset(buf,0,strlen(buf));
     gen_rand_expr();
-
+    buf[idx] = '\0';
     sprintf(code_buf, code_format, buf);
 
     FILE *fp = fopen("/tmp/.code.c", "w");
@@ -45,7 +87,7 @@ int main(int argc, char *argv[]) {
     assert(fp != NULL);
 
     int result;
-    fscanf(fp, "%d", &result);
+    if(fscanf(fp, "%d", &result));
     pclose(fp);
 
     printf("%u %s\n", result, buf);
